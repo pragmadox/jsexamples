82ba54adfb2c1e618e11f1d11481db9becbceac6
Jay Price <jxprice@live.com>
2017-07-12 10:34:01 -0400

    Re-worked the js-slideshow.

--- js-slideshow/js/slide.js
+++ js-slideshow/js/slide.js
@@ -1,21 +1,24 @@
-var imgArr = new Array();
+var imgArr = [];
 var i = 0;
 
 var thumbnails = document.getElementById("thumbnails");
+var bicPic = document.getElementById("bigPic");
 
 function loadImages() {
-	for(i=1; i<20; i++) {
-	imgArr[i] = new Image();
-	imgArr[i].src = 'images/eagle_rock20161105-' + i + '-.jpeg';
-	imgArr[i].addEventListener("click", swap);
-	thumbnails.appendChild(imgArr[i]);
-  }
+	largeImage = new Image();
+	largeImage.src = 'images/eagle_rock20161105-' + 1 + '-.jpeg';
+	bigPic.appendChild(largeImage);
 	
+		for(i=1; i<20; i++) {
+		imgArr[i] = new Image();
+		imgArr[i].src = 'images/eagle_rock20161105-' + i + '-.jpeg';
+		imgArr[i].addEventListener("click", swap);
+		thumbnails.appendChild(imgArr[i]);
+		}
 }
 
 function swap() {
-	var actual = new Image();
-	document.getElementById("bigPic").innerHTML = '<img src="' + this.src + '">';
+	largeImage.src = this.src;
 }
 
 loadImages();
